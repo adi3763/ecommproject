3126fee8f4cd9bd51ef45fb275c918c6ec718777
Aditya Anand <aanand@marquistech.com>
2025-05-24 16:44:50 +0530

    Add TrustProxies middleware to fix mixed content

---
+++ app/Http/Middleware/TrustProxies.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Illuminate\Http\Middleware\TrustProxies as Middleware;
+use Illuminate\Http\Request;
+
+class TrustProxies extends Middleware
+{
+    /**
+     * The trusted proxies for this application.
+     *
+     * Use '*' to trust all proxies (like Render, Heroku, Vercel).
+     */
+    protected $proxies = '*';
+
+    /**
+     * The headers that should be used to detect proxies.
+     */
+    protected $headers = Request::HEADER_X_FORWARDED_ALL;
+}
