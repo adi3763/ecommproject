17d3b43253b7f0a7d4080c3c240fe7988820d580
Aditya Anand <aanand@marquistech.com>
2025-05-27 10:51:53 +0530

    ..

--- public/JS/addSubCategory.js
+++ public/JS/addSubCategory.js
@@ -15,7 +15,6 @@ submitButton.addEventListener("click", async function (e) {
 
     const result = await response.json();
     if(result.status == 200){
-        window.location.href = "/admin/categoryPage";
         alert(result.message);
         form.reset();
     } else {
